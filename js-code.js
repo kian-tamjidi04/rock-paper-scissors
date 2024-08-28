a93f8349b65d68984e2912b0d4217eeb0f75bc7d
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2024-08-28 14:30:36 +0100

    Add playGame function to play multiple rounds

--- js-code.js
+++ js-code.js
@@ -23,23 +23,38 @@ function getHumanChoice() {
     return response;
 }
 
-function playRound(human, computer) {
-    // Logic for the game
-    if (human == computer) {
-        console.log("You both picked " + human + ". It's a tie!");
-    } else if ((human == "rock" && computer == "scissors") || (human == "scissors" && computer == "paper") || (human == "paper" && computer == "rock")) {
-        console.log("You picked " + human + ", computer picked " + computer + ". You win!!");
-        humanScore++;
+function playGame() {
+    function playRound(human, computer) {
+        // Logic for the game
+        if (human == computer) {
+            console.log("You both picked " + human + ". It's a tie!");
+        } else if ((human == "rock" && computer == "scissors") || (human == "scissors" && computer == "paper") || (human == "paper" && computer == "rock")) {
+            console.log("You picked " + human + ", computer picked " + computer + ". You win!!");
+            humanScore++;
+        } else {
+            console.log("You picked " + human + ", computer picked " + computer + ". You lose :(");
+            computerScore++;
+        }
+        console.log(humanScore, computerScore);
+    }
+    
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+
+    // Keeps playing the game for 5 rounds
+    while (humanScore + computerScore < 5) {
+        playRound(humanChoice, computerChoice);
+    }
+
+    // Declares a winner
+    if (humanScore > computerScore) {
+        alert("You won!")
     } else {
-        console.log("You picked " + human + ", computer picked " + computer + ". You lose :(");
-        computerScore++;
+        alert("The computer won")
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice);
+playGame();
